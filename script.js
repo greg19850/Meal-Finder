8a7d73b6cf30c2ac8a782eca7c44b672506f52ef
Greg <greg19850@gmail.com>
2022-09-01 20:30:37 +0100

    Created searchMeal function, which fetch data from API, and list meals with photo, based on searched phrase

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ const randomBtn = document.getElementById('random');
 const resultHeading = document.getElementById('result-heading')
 const mealsEl = document.getElementById('meals')
 const single_mealEl = document.getElementById('single-meal');
+
+// Search meal and fetch API
+
+function searchMeal(e) {
+  e.preventDefault();
+
+  const mealName = search.value
+
+  if (mealName.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p> There are no results for '${mealName}'. Try again!`;
+        } else {
+          resultHeading.innerHTML = `<h2>Search results for '${mealName}' :</h2>`
+
+          mealsEl.innerHTML = data.meals.map(meal => `
+          <div class='meal'>
+            <div clas='meal-info' data-mealID='${meal.idMeal}'>
+              <h3>${meal.strMeal}</h3>
+            </div>
+            <img src='${meal.strMealThumb}' alt='${meal.strMeal}' />
+          </div>
+          `)
+            .join('')
+        }
+      })
+    search.value = ''
+  } else alert('Please enter meal name!')
+}
+
+
+submitBtn.addEventListener('click', searchMeal)
