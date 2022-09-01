fd1c0185f62371fb5d0c8653ff3585c7ddd7e2b3
Greg <greg19850@gmail.com>
2022-09-01 19:31:58 +0100

    Create variables in script.js, for all necessary DOM elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const search = document.getElementById('search');
+const submitBtn = document.querySelector('.search-btn');
+const randomBtn = document.getElementById('random');
+const resultHeading = document.getElementById('result-heading')
+const mealsEl = document.getElementById('meals')
+const single_mealEl = document.getElementById('single-meal');
